841269c431a72374dfb32f29ff399b53a65b4edb
Felipe-Cancio <cancio.acad@gmail.com>
2022-06-10 13:35:30 +0000

    Alteração 10/06

--- atv_Mat.cpp
+++ atv_Mat.cpp
@@ -1,16 +1,74 @@
 #include <cstdlib>
 #include <stdio.h>
 
+void mat_q1(int **m, int qtdLinhas, int qtdColunas){ // Função p/ transformar matriz em vetor
+    char opc = 'c'; // Forçando a condição de coluna p/ testes
+
+    printf("Você quer transformar linhas ou colunas ( L/C )? ");
+    //scanf("%c", &opc);
+
+    printf("\nMatriz tranformada em vetor: \n");
+
+    if (opc == 'L' || opc == 'l'){ // Lógica p/ linhas
+        for (int i = 0; i < qtdLinhas; i++) {
+            for (int j = 0; j < qtdColunas; j++) {
+                printf("%d ", m[i][j]);
+            }
+        }
+    }
+    
+    else if (opc == 'C' || opc == 'c'){ // Lógica p/ colunas 
+        for (int i = 0; i < qtdColunas; i++) {
+            for (int j = 0; j < qtdLinhas; j++) {
+                printf("%d ", m[j][i]);
+            }
+        }
+    } 
+    printf("\n");
+}
+
+void mat_q2(int **m, int qtdLinhas, int qtdColunas){
+    int qtd;
+    char opc = 'l';
+
+    //printf("Você deseja adicionar uma linha ou uma coluna ( L/C )? ");
+    //scanf("%d", &opct);
+
+    printf("Quantas você deseja adicionar? ");
+    scanf("%d", &qtd);
+
+    printf("\nlog1");
+
+    if (opc == 'l'){
+        printf("\nlog2");
+        m = (int**)realloc(m, (qtdLinhas + qtd)*sizeof(int*));
+        for (int i = qtdLinhas; i < qtd; i++) {
+            for (int j = 0; j < qtd; j++) {
+                int val;
+                printf("\nlog3");
+                printf("Digite o novo valor do elemento [%d][%d] ", i, j);
+                scanf("%d", &val);
+                m[i][j] = val;
+            }
+        }
+
+    }
+
+    /*else if (opc == 'C' || opc == 'c'){
+
+    }*/
+}
+
 void mat_q4(int **m, int qtdLinhas, int qtdColunas){ // passar como parametros o vetorzao e o tamanho dele
     int maior = m[0][0], menor = m[0][0], nPares=0, nImpares=0;
     
     for (int l = 0; l < qtdLinhas; l++) {
-        for(int c=1; c<qtdColunas; c++){
+        for(int c=0; c<qtdColunas; c++){
             if (maior < m[l][c]) // Comparação de um elemento com o próximo p/ saber qual o maior
                 maior = m[l][c];
             if (menor > m[l][c]) // comparação de um elemento com o próximo p/ saber qual o menor
                 menor = m[l][c];
-            if (m[l][c-1]%2 == 0) // verifica se é par ou impar
+            if ((m[l][c] % 2) == 0) // verifica se é par ou impar
                 nPares++; // caso seja par incrementar o contador de nº pares
             else
                 nImpares++; // caso contrario incrementa o contador de nº ímpares
@@ -19,7 +77,7 @@ void mat_q4(int **m, int qtdLinhas, int qtdColunas){ // passar como parametros o
     printf("O maior eh %d e o menor eh %d, tem %d numeros pares e %d impares\n", maior, menor, nPares, nImpares);
 }
 
-void printM(int **m, int qtdLinhas, int qtdColunas){
+void printM(int **m, int qtdLinhas, int qtdColunas){ // Função p/ imprimir matrizes
     for (int i = 0; i < qtdLinhas; i++) {
         for (int j = 0; j < qtdColunas; j++) {
             printf("%d ", m[i][j]);
@@ -28,7 +86,7 @@ void printM(int **m, int qtdLinhas, int qtdColunas){
     }
 }
 
-int** alocaM(int **m, int qtdLinhas, int qtdColunas){
+int** alocaM(int **m, int qtdLinhas, int qtdColunas){ // Função p/ alocar matrizes
 
     m = (int**)calloc(qtdLinhas, sizeof(int*));
 
@@ -39,7 +97,7 @@ int** alocaM(int **m, int qtdLinhas, int qtdColunas){
     return m;
 }
 
-void lerMatriz(int **m, int qtdLinhas, int qtdColunas){
+void lerMatriz(int **m, int qtdLinhas, int qtdColunas){ // Função p/ preencher matrizes com dados do usuário
 
     for (int i = 0; i < qtdLinhas; i++) {
         for (int j = 0; j < qtdColunas; j++) {
@@ -52,10 +110,10 @@ void lerMatriz(int **m, int qtdLinhas, int qtdColunas){
 
 }
 
-typedef struct Matriz{
+typedef struct matriz{
     int **m;
-    int qtdLinhas;
-    int qtdColunas;
+    int qtdLinhas; // Vulgo qntd de vetores
+    int qtdColunas; //  Vulgo tamanho dos vetores
 
 } Matriz;
 
@@ -76,7 +134,7 @@ int main(){
         scanf("%d", &(m[i].qtdColunas));
 
         m[i].m = alocaM(m[i].m, m[i].qtdLinhas, m[i].qtdColunas); // alocação da matriz na struct
-        //lerMatriz(m[i].m, m[i].qtdLinhas, m[i].qtdColunas);
+        lerMatriz(m[i].m, m[i].qtdLinhas, m[i].qtdColunas);
     }
 
     printf("\n---IMPRIMINDO AS MATRIZES---\n");
@@ -84,7 +142,10 @@ int main(){
     for (int i = 0; i < qtd; i++) { // Fazer a impressão das matrizes
         printf("\n");
         printM(m[i].m, m[i].qtdLinhas, m[i].qtdColunas);
-        mat_q4(m[i].m, m[i].qtdLinhas, m[i].qtdColunas);
+        //mat_q4(m[i].m, m[i].qtdLinhas, m[i].qtdColunas);
+        //mat_q1(m[i].m, m[i].qtdLinhas, m[i].qtdColunas);
+        mat_q2(m[i].m, m[i].qtdLinhas, m[i].qtdColunas);
+        printM(m[i].m, m[i].qtdLinhas, m[i].qtdColunas);
     }
 
 
